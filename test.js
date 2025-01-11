4795ebce6e04a2802600987b3c9e6a4bd6c52dea
pashto-programing-zone <58067-s020@hu.edu.pk>
2025-01-11 14:49:15 +0500

    conversion commit

--- test.js
+++ test.js
@@ -1,16 +1,15 @@
 console.log("zain")
-console.log("zain")
-console.log("zain")
-console.log("zain")
-console.log("zain")
-console.log("zain")
-console.log("zain")
-console.log("zain")
-console.log("zain")
-console.log("zain")
-console.log("zain")
-console.log("zain")
-console.log("zain")
-console.log("zain")
-console.log("zain")
-console.log("zain")
+console.log(3+3)
+console.log(typeof null);
+let age = 20;
+console.log(age)
+console. log(typeof age);
+let state;
+console.log(state)
+// was ba za 1 table jorom
+let accountAge=(14)
+let accountState=("swat")
+let accountNumber=(1234)
+let accountNic=(15601)
+let accountEmail=("zain@gamail.com")
+console.table([accountAge,accountState,accountNumber,accountNic,accountEmail])
