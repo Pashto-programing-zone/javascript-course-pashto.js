1fa530d63021c1505ad27a4dd009fb09312fded6
Pashto-programing-zone <58067-s020@hu.edu.pk>
2025-02-17 21:16:55 +0500

    commit strings

--- strings.js
+++ strings.js
@@ -3,3 +3,22 @@ const githubRepo="2Repository"
  //console.log(name+ githubRepo)
 
  console.log(`my youtube chanle name is ${name} and i have ${githubRepo}`)
+
+ const gameName =new String('pubGlover');
+ console.log(gameName.__proto__)
+ console.log(gameName)
+ console.log(gameName.length)
+ console.log(gameName.toUpperCase());
+ console.log(gameName.charAt(2))
+ console.log(gameName.indexOf('G'));
+ const newMethold =gameName.substring(0,2);
+ console.log(newMethold)
+ const anotherString=gameName.slice(2,6)
+ console.log(anotherString)
+
+ const url="https//:zain.com/khan20%chudry"
+ console.log(url)
+ console.log(url.replace('20%','_'))
+
+
+
