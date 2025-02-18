17e836339077f15a4db67f4ffbe709bbebe3f389
Pashto-programing-zone <58067-s020@hu.edu.pk>
2025-02-18 16:40:12 +0500

    commit number and math

---
+++ num_and_math.js
@@ -0,0 +1,39 @@
+const score=400
+console.log(score)
+
+const balance = new Number(100)
+console.log(balance)
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+console.log(typeof balance)
+  
+const otherNumber=409.989958038
+console.log(otherNumber.toPrecision(4));
+
+const moreNumbers=1000000
+console.log(moreNumbers.toLocaleString('en-PK'));
+
+
+
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^MAth^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+
+
+console.log(Math)
+console.log(Math.abs(-5));
+
+console.log(Math.round(5.7));
+console.log(Math.ceil(5.7));
+console.log(Math.floor(5.7));
+
+console.log(Math.sqrt(25));
+console.log(Math.pow(2,3));
+console.log(Math.min(9,4,7,2,3));
+console.log(Math.max(9,4,7,2,3));
+
+
+
+
+
