51fc1edb4074c015fff6689f630f78b9ba79aa40
Pashto-programing-zone <58067-s020@hu.edu.pk>
2025-03-28 23:41:09 +0500

    iife commit

---
+++ 03_basics/04_iifi.js
@@ -0,0 +1,9 @@
+// immediately invoked function expressions( IIFE) 
+
+// IIFE function mang wali use kw ? zaka e use kw che globle scope da pulotion da waji masali raze zaka mang iife function use kw .
+
+
+(function chai(){
+    console.log(`DB CONNECTED`);
+    
+}())
