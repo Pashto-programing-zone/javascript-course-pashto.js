615eb50434eeac5ceddc6a3a52a6bf0d183228b1
Pashto-programing-zone <58067-s020@hu.edu.pk>
2025-03-29 00:10:56 +0500

    commit

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -3,7 +3,37 @@
 // IIFE function mang wali use kw ? zaka e use kw che globle scope da pulotion da waji masali raze zaka mang iife function use kw .
 
 
-(function chai(){
-    console.log(`DB CONNECTED`);
+// (function chai(){
+//     console.log(`DB CONNECTED`);
     
-}())
+// }())
+
+// ******************************** example pa arrow function***************************
+
+
+// (  ()=>{
+// console.log(`DB CONNECTED TWO`);
+
+// }  )()
+
+// =========================================
+
+// che kala mng dwa function yozai leko no lazimi bae pa semicolon bani juda kw  example
+
+// (function chai(){
+//         console.log(`DB CONNECTED`);
+        
+//     }());    // pa line ki mang pa semicolon bani juda kro dada javascrift roles d aw d na baghair kar kae
+
+//     (  ()=>{
+//         console.log(`DB CONNECTED TWO`);
+        
+//         }  )()
+
+//========================================================
+// arrow function veriable hm expect kae
+
+        (  (name)=>{
+            console.log(`DB CONNECTED TWO ${name}`);
+            
+            }  )(`zain`)
